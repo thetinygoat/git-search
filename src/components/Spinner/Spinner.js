b6e475143ee9485314ada8416d79e173857f63d0
Sachin Saini <iamsainisachin@gmail.com>
2018-12-12 00:59:53 +0530

    add spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Spinner.css'
+
+const spinner = () => {
+    return (
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    )
+}
+export default spinner;
